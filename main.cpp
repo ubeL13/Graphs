1adcce25f4b3c5d40f7eb05fbb129e421ff17085
UbeL13 <u.b.2004@yandex.ru>
2024-06-04 15:23:45 +0400

    create class

--- main.cpp
+++ main.cpp
@@ -14,11 +14,9 @@ int main() {
 
     std::cout << "Vertices added." << std::endl;
 
-    // Проверка наличия вершины
     std::cout << "Graph has vertex 'A': " << graph.has_vertex('A') << std::endl;
     std::cout << "Graph has vertex 'Z': " << graph.has_vertex('Z') << std::endl;
 
-    // Добавление ребер
     graph.add_edge('A', 'B', 1.0);
     graph.add_edge('A', 'C', 4.0);
     graph.add_edge('B', 'C', 2.0);
@@ -28,7 +26,6 @@ int main() {
 
     std::cout << "Edges added." << std::endl;
 
-    // Проверка наличия ребра
     std::cout << "Graph has edge from 'A' to 'B': " << graph.has_edge('A', 'B') << std::endl;
     std::cout << "Graph has edge from 'A' to 'E': " << graph.has_edge('A', 'E') << std::endl;
 
